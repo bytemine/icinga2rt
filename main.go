a23fb9cf6b584f0bbfd5d341947a88f4218b42c7
Ruben Schuller <schuller@bytemine.net>
2018-10-10 16:53:33 +0200

    version 0.2.1

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 	"github.com/bytemine/icinga2rt/rt"
 )
 
-const version = "0.2.0"
+const version = "0.2.1"
 const icingaQueueName = "icinga2rt"
 
 var writeExample = flag.Bool("example", false, "write example configuration file as icinga2rt.json.example to current directory")
